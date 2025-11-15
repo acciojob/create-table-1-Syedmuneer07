7a902c9d9a91d14164e17fd0e4c098a153960b67
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-16 00:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function insert_Row() {
   const	newRow=table.insertRow(0);
 
 	
-	const cell1=newRow.insertcell(0);
+	const cell1=newRow.insertCell(0);
 	
-	const cell2=newRow.insertcell(1);
+	const cell2=newRow.insertCell(1);
 
 	cell1.innerHTML="New Cell1"; 
 	cell2.innerHTML="New Cell2";
